1b3aef11401a8e7879a5e50ae78c072e46efbfc8
Dabo Ross <daboross@daboross.net>
2014-04-02 19:31:31 -0700

    Implement init for Platform, also make initiation synchronized

--- src/main/java/org/ingrahamrobotics/robottables/util/Platform.java
+++ src/main/java/org/ingrahamrobotics/robottables/util/Platform.java
@@ -2,24 +2,34 @@ package org.ingrahamrobotics.robottables.util;
 
 public class Platform {
 
+    private static final Object LOCK = new Object();
     private static boolean ready = false;
     private static boolean onRobot = false;
 
     public static boolean onRobot() {
-        if (!ready) {
-            init();
+        synchronized (LOCK) {
+            if (!ready) {
+                init();
+            }
         }
         return onRobot;
     }
 
     private static void init() {
-        // One of these (or a combination thereof) will let me figure out if we're running on the robot
-        System.getProperty("java.version");
-        System.getProperty("java.vendor");
-        System.getProperty("java.compiler");
-        System.getProperty("os.name");
+        try {
+            Class.forName("java.util.ArrayList");
+            onRobot = false;
+        } catch (ClassNotFoundException ex) {
+            onRobot = true;
+        }
+        System.out.println("Initiated onRobot=" + onRobot);
+//        // One of these (or a combination thereof) will let me figure out if we're running on the robot
+//        System.getProperty("java.version");
+//        System.getProperty("java.vendor");
+//        System.getProperty("java.compiler");
+//        System.getProperty("os.name");
+//        onRobot = false;
 
         ready = true;
-        onRobot = false;
     }
 }
