493045864e3c016dfde620088bc72b0854b956b0
Dabo Ross <daboross@daboross.net>
2015-02-16 20:38:42 -0800

    Implement getTableSet for TablesInterfaceHandler

--- src/main/java/org/ingrahamrobotics/robottables/TablesInterfaceHandler.java
+++ src/main/java/org/ingrahamrobotics/robottables/TablesInterfaceHandler.java
@@ -1,6 +1,8 @@
 package org.ingrahamrobotics.robottables;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -144,6 +146,10 @@ public class TablesInterfaceHandler implements RobotTablesClient, InternalTableH
 
     public InternalTable getTable(final String tableName) {
         return tableMap.get(tableName);
+    }
+
+    public Collection<? extends RobotTable> getTableSet() {
+        return Collections.unmodifiableCollection(tableMap.values());
     }
 
     public RobotProtocol getProtocolHandler() {
